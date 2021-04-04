691e9a925d2171b89d3841277d56ce6ff5303f3a
buriedsignals <tom.vaillant@icloud.com>
2021-04-04 14:08:15 +0200

    navigation

--- components/summary-component.js
+++ components/summary-component.js
@@ -34,17 +34,16 @@ class SummaryComponent extends React.Component {
         <span>Table of Contents</span>
         <ul>
           <li onClick={() => this.scrollTo(1)}>Introduction</li>
-          <li onClick={() => this.scrollTo(2)}>200BC - 500AD</li>
-          <li onClick={() => this.scrollTo(3)}>500AD - 1300AD</li>
-          <li onClick={() => this.scrollTo(4)}>1500AD - 1850AD</li>
-          <li onClick={() => this.scrollTo(5)}>South Yemen</li>
-          <li onClick={() => this.scrollTo(6)}>North Yemen</li>
-          <li onClick={() => this.scrollTo(7)}>Nasser</li>
-          <li onClick={() => this.scrollTo(8)}>Soviet Collapse</li>
-          <li onClick={() => this.scrollTo(9)}>2010 - 2020</li>
-          <li onClick={() => this.scrollTo(10)}>Iran allegations</li>
-          <li onClick={() => this.scrollTo(11)}>War on food</li>
-          <li onClick={() => this.scrollTo(12)}>Moving forward</li>
+          <li onClick={() => this.scrollTo(2)}>200BC - 500AD (Christianity)</li>
+          <li onClick={() => this.scrollTo(3)}>500 - 1300 (Islam)</li>
+          <li onClick={() => this.scrollTo(4)}>1500 - 1850 (Persia) </li>
+          <li onClick={() => this.scrollTo(5)}>1900s (South Yemen)</li>
+          <li onClick={() => this.scrollTo(6)}>1900s (North Yemen)</li>
+          <li onClick={() => this.scrollTo(7)}>1950s (Soviet Collapse)</li>
+          <li onClick={() => this.scrollTo(200)}>2000s (Civil War)</li>
+          <li onClick={() => this.scrollTo(9)}>2010 (Iran)</li>
+          <li onClick={() => this.scrollTo(23)}>2015 (War on food)</li>
+          <li onClick={() => this.scrollTo(11)}>Help</li>
         </ul>
       </div>
     </div>  
